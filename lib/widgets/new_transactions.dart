7108e36e20f65b2616067e51ed9860f4f3324f00
PetarCRO <petar.prenc@gmail.com>
2022-09-22 10:49:59 +0200

    date picker added

--- lib/widgets/new_transactions.dart
+++ lib/widgets/new_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTx;
@@ -14,7 +15,9 @@ class _NewTransactionState extends State<NewTransaction> {
 
   final amountController = TextEditingController();
 
-  void submitData() {
+  late DateTime _selectedDate;
+
+  void _submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
 
@@ -26,6 +29,22 @@ class _NewTransactionState extends State<NewTransaction> {
     Navigator.of(context).pop();
   }
 
+  void _presentDatePicker() {
+    showDatePicker(
+            context: context,
+            initialDate: DateTime.now(),
+            firstDate: DateTime(2022),
+            lastDate: DateTime.now())
+        .then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -33,60 +52,69 @@ class _NewTransactionState extends State<NewTransaction> {
       child: Container(
         padding: const EdgeInsets.all(10),
         margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 6),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: 'Title'),
-              controller: titleController,
-              onSubmitted: (_) => submitData(),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Amount'),
-              controller: amountController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => submitData(),
-            ),
-            Container(
-              height: 100,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('No date chosen'),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Choose date'),
+        child: Container(
+          height: 430,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Title'),
+                controller: titleController,
+                onSubmitted: (_) => _submitData(),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount'),
+                controller: amountController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) => _submitData(),
+              ),
+              Container(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      _selectedDate == null
+                          ? 'No date chosen'
+                          : DateFormat('dd/MM/yyyy').format(_selectedDate),
+                    ),
+                    ElevatedButton(
+                      onPressed: _presentDatePicker,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        textStyle: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      child: const Text('Choose date'),
+                    ),
+                  ],
+                ),
+              ),
+              Divider(
+                height: 90,
+                thickness: 1.5,
+                indent: 20,
+                endIndent: 20,
+              ),
+              Center(
+                child: Container(
+                  width: 250,
+                  height: 35,
+                  child: ElevatedButton(
+                    onPressed: _submitData,
+                    child: const Text('Add Transaction'),
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.red,
                       textStyle: TextStyle(fontWeight: FontWeight.bold),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Divider(
-              height: 90,
-              thickness: 1.5,
-              indent: 20,
-              endIndent: 20,
-            ),
-            Center(
-              child: Container(
-                width: 250,
-                height: 35,
-                child: ElevatedButton(
-                  onPressed: submitData,
-                  child: const Text('Add Transaction'),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.red,
-                    textStyle: TextStyle(fontWeight: FontWeight.bold),
-                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
